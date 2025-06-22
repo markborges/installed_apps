dd71ec5e7422988812382ccbfa31d42c6bb852a4
Marcelo Borges <markborges@gmail.com>
2025-06-21 22:58:39 -0400

    Plugin update to Listen to Install and Uninstall Events

--- android/src/main/kotlin/com/sharmadhiraj/installed_apps/InstalledAppsPlugin.kt
+++ android/src/main/kotlin/com/sharmadhiraj/installed_apps/InstalledAppsPlugin.kt
@@ -1,8 +1,10 @@
 package com.sharmadhiraj.installed_apps
 
+import MyPackageReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.content.pm.ApplicationInfo
 import android.net.Uri
@@ -19,21 +21,43 @@ import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 import java.util.Locale.ENGLISH
+import android.content.BroadcastReceiver
+import io.flutter.plugin.common.EventChannel
+
 
 class InstalledAppsPlugin : MethodCallHandler, FlutterPlugin, ActivityAware {
-
     private lateinit var channel: MethodChannel
+    private lateinit var eventChannel: EventChannel
     private var context: Context? = null
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         context = binding.applicationContext
         channel = MethodChannel(binding.binaryMessenger, "installed_apps")
         channel.setMethodCallHandler(this)
+
+        // LISTEN APP INSTALL AND UNINSTALL EVENTS
+        val EVENT_CHANNEL = "com.sharmadhiraj.installed_apps/app_monitor"
+
+        eventChannel = EventChannel(binding.binaryMessenger, EVENT_CHANNEL)
+        eventChannel.setStreamHandler(object : EventChannel.StreamHandler {
+            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                val receiver = MyPackageReceiver(events)
+                val intentFilter = IntentFilter()
+                intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED)
+                intentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED)
+                intentFilter.addDataScheme("package")
+                binding.applicationContext.registerReceiver(receiver, intentFilter)
+            }
+
+            override fun onCancel(arguments: Any?) {}
+        })
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
     }
 
     override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {
